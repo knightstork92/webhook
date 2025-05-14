fdff49510319729b51615b4b2c5c9eb3246f3467
Knight Stork <khanhvq04890iphone@gmail.com>
2025-05-14 22:49:19 +0700

    fix: clean node_modules for proper deploy

--- firebase.js
+++ firebase.js
@@ -1,7 +1,10 @@
 const admin = require("firebase-admin");
 
+const serviceAccountBase64 = process.env.FIREBASE_SERVICE_ACCOUNT;
+const serviceAccount = JSON.parse(Buffer.from(serviceAccountBase64, "base64").toString("utf8"));
+
 admin.initializeApp({
-  credential: admin.credential.cert(require("./serviceAccountKey.json")),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
