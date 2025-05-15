6d59a3b45cc56fc5b5d6c9eb7b4131955a05282b
Knight Stork <khanhvq04890iphone@gmail.com>
2025-05-16 00:07:51 +0700

    Fix: thêm xử lý private_key cho Render 1

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const admin = require("firebase-admin");
 const { google } = require("googleapis");
 
-// === Khởi tạo Firebase Admin ===
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, "base64").toString("utf8")
+);
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+
 const db = admin.firestore();
 
 // === Cấu hình server Express ===
